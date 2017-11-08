92f7559e00668e62f8155464af2e8dca1a61b27b
underscorenico <vinuesa.nico@gmail.com>
2017-11-08 22:42:45 +0100

    Implement request handlers in quote generator

--- quote-generator/src/main/java/io/vertx/workshop/quote/RestQuoteAPIVerticle.java
+++ quote-generator/src/main/java/io/vertx/workshop/quote/RestQuoteAPIVerticle.java
@@ -21,12 +21,8 @@ public class RestQuoteAPIVerticle extends AbstractVerticle {
   public void start() throws Exception {
     vertx.eventBus().<JsonObject>consumer(GeneratorConfigVerticle.ADDRESS)
         .handler(message -> {
-          // TODO Populate the `quotes` map with the received quote
-          // Quotes are json objects you can retrieve from the message body
-          // The map is structured as follows: name -> quote
-          // ----
-
-          // ----
+            JsonObject quote = message.body();
+            quotes.put(quote.getString("name"), quote);
         });
 
 
@@ -35,19 +31,21 @@ public class RestQuoteAPIVerticle extends AbstractVerticle {
           HttpServerResponse response = request.response()
               .putHeader("content-type", "application/json");
 
-          // TODO
-          // The request handler returns a specific quote if the `name` parameter is set, or the whole map if none.
-          // To write the response use: `request.response().end(content)`
-          // Responses are returned as JSON, so don't forget the "content-type": "application/json" header.
-          // If the symbol is set but not found, you should return 404.
-          // Once the request handler is set,
+            String company = request.getParam("name");
+            if (company == null) {
+                
+                String content = Json.encodePrettily(quotes);
+                response
+                        .end(content);
+            } else {
 
-          response
-              .end(Json.encodePrettily(quotes));
-
-          // ----
-
-          // ----
+                JsonObject quote = quotes.get(company);
+                if (quote == null) {
+                    response.setStatusCode(404).end();
+                } else {
+                    response.end(quote.encodePrettily());
+                }
+            }
         })
         .listen(config().getInteger("http.port"), ar -> {
           if (ar.succeeded()) {
